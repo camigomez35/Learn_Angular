20d97f9c2a0095a8bf8c6b05eeeb7daafe3cbbe1
camigomez35 <camigomez35@gmail.com>
2017-08-20 13:17:43 -0500

    Learn Angular basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { VariablesComponent } from './component/variables/variables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VariablesComponent
   ],
   imports: [
     BrowserModule,
